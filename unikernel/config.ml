29953e3af4a513a5d458cd523be50115a80f982a
Sid Sabhnani <ssabhnani@utexas.edu>
2024-12-10 19:08:06 -0600

    hail mary

--- unikernel/config.ml
+++ unikernel/config.ml
@@ -1,26 +1,28 @@
 open Mirage
 
-let ipv4_config : ipv4_config =
-  {
-    network = Ipaddr.V4.Prefix.of_string_exn "192.168.122.2/24";
-    gateway = Some (Ipaddr.V4.of_string_exn "192.168.122.1");
-  }
-
 let stack =
   if "xen" = Sys.getenv "MODE" then
+    let ipv4_config : ipv4_config =
+      {
+        network = Ipaddr.V4.Prefix.of_string_exn "192.168.122.2/24";
+        gateway = Some (Ipaddr.V4.of_string_exn "192.168.122.1");
+      } in
     generic_stackv4v6 ~ipv4_config default_network
   else generic_stackv4v6 default_network
+(* let http_server = cohttp_server @@ conduit_direct ~tls:false stack *)
 
-let http_server = cohttp_server @@ conduit_direct ~tls:false stack
-
-let packages = [
-  package "h2";
-  package "ocaml-protoc-plugin";
-  package "mirage-crypto-rng";
-]
-
-let main =
-  main ~packages "Unikernel.RaftServer" (time @-> pclock @-> http @-> job)
+let main = 
+ let packages =
+  [
+    package "ocaml-protoc-plugin";
+    package "mirage-crypto-rng";
+    package "h2-lwt";
+    package "h2-mirage";
+    package "grpc";
+    package "grpc-lwt";
+    package "tls-mirage";
+  ]
+in main "Unikernel.RaftServer" ~packages (random @-> time @-> pclock @-> stackv4v6 @-> job)
 
 let () =
-  register "raft" [ main $ default_time $ default_posix_clock $ http_server ]
+  register "raft" [ main $ default_random $ default_time $ default_posix_clock $ stack ]
